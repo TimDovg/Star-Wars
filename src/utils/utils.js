053ad81c8002b5ffce6a839d3ce00f0675b78cef
TimDovg <timdovg@gmail.com>
2020-11-12 07:20:48 +0200

    fixed firebase Mixed Content bug (http -> https)

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -2,4 +2,5 @@ import axios from 'axios'
 
 export const getDigitsFromString = url => url && url.replace(/\D/g, '')
 export const addSpacesToNumber = num => String(num).trim().split('').reverse().join('').replace(/\d{3}/g, m => `${m} `).split('').reverse().join('').trim()
-export const promisifyURLS = urls => urls.map(url => axios(url))
+// fixed firebase Mixed Content bug (http -> https)
+export const promisifyURLS = urls => urls.map(url => axios(url.replace(/^http/, 'https')))
